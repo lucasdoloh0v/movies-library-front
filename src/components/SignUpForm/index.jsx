b7eb2a2f38ecc0f4b4ad87789f0ca008509ce754
Lucas Alves <lucasdolohov@gmail.com>
2023-09-20 00:34:32 -0300

    feat: user registration integration

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,21 +1,67 @@
 import { FiLock, FiMail, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
 
+import { api } from '../../services/api';
+
 import { Container } from './styles';
 import { Input } from '../Input';
 import { Button } from '../Button';
+import { useState } from 'react';
 
 export function SignUpForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      return alert('preencha todos os campos');
+    }
+
+    try {
+      await api.post('/users', { name, email, password });
+      alert('Usuário cadastrado com sucesso');
+
+      navigate('/');
+    } catch (error) {
+      if (error.response) {
+        return alert(error.response.data.message);
+      } else {
+        return alert('Não foi possivel cadastrar o usuário');
+      }
+    }
+  };
 
   return (
     <Container>
       <h3>Crie sua conta</h3>
-      <Input icon={FiUser} placeholder="Nome" />
-      <Input icon={FiMail} placeholder="E-mail" />
-      <Input icon={FiLock} placeholder="Senha" type="password" />
+      <Input
+        icon={FiUser}
+        placeholder="Nome"
+        type="text"
+        value={name}
+        onChange={({ target }) => setName(target.value)}
+      />
+      <Input
+        icon={FiMail}
+        placeholder="E-mail"
+        type="email"
+        value={email}
+        onChange={({ target }) => setEmail(target.value)}
+      />
+      <Input
+        icon={FiLock}
+        placeholder="Senha"
+        type="password"
+        value={password}
+        onChange={({ target }) => setPassword(target.value)}
+      />
 
-      <Button title="Cadastrar" className="registerBtn" />
+      <Button title="Cadastrar" type="submit" className="registerBtn" onClick={handleSignUp} />
       <Button
         title="Voltar para o login"
         variant="text-button"
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 
-// import { AuthRoutes } from './auth.routes';
-import { AuthenticatedRoutes } from './athenticated.routes';
+import { AuthRoutes } from './auth.routes';
+// import { AuthenticatedRoutes } from './athenticated.routes';
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
-      {/* <AuthRoutes /> */}
-      <AuthenticatedRoutes />
+      <AuthRoutes />
+      {/* <AuthenticatedRoutes /> */}
     </BrowserRouter>
   );
 }
