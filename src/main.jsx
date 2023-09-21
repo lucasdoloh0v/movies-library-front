1c56c059c437c4c4a58093d3815cd4d25dc69b52
Lucas Alves <lucasdolohov@gmail.com>
2023-09-21 20:22:51 -0300

    feat: auth hook

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
 import { AppRoutes } from './routes';
+import { AuthProvider } from './hooks/auth';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <AppRoutes />
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
 );
