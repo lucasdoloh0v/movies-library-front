cf32f8590f6e899190e3fee3637b08d99ab829d0
Lucas Alves <lucasdolohov@gmail.com>
2023-09-27 20:23:14 -0300

    feat: movie details integration

--- src/pages/MovieDetails/index.jsx
+++ src/pages/MovieDetails/index.jsx
@@ -1,53 +1,75 @@
+import { useCallback, useEffect, useState } from 'react';
 import { FiArrowLeft, FiClock } from 'react-icons/fi';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import avatarPlaceolder from '../../assets/avatar_placeholder.svg';
 
 import { Container, Content, Tags } from './styles';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { Rating } from '../../components/Rating';
 import { Tag } from '../../components/Tag';
+import { api } from '../../services/api';
+import { useAuth } from '../../hooks/auth';
 
 export function MovieDetails() {
-  const navigate = useNavigate();
+  const [movie, setMovie] = useState({});
 
-  const tags = ['suspense', 'drama'];
-  const description =
-    'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis vero ratione recusandae illum architecto harum accusamus dolor, quidem consequuntur facere fugiat cum, cumque quasi quisquam a tempore tenetur ab et? Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum debitis officia dolores itaque optio! Sapiente, nostrum perferendis? Dicta sunt ipsum commodi fuga placeat voluptate totam, quas a veniam. Aut, cupiditate. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore temporibus deserunt cumque pariatur ex officiis mollitia tempora architecto. Possimus facere minima dolorum sed officiis commodi aspernatur autem inventore fuga culpa. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae corporis tempore cupiditate quis facilis officia quia, cum laboriosam voluptas quas eos iusto laudantium sequi voluptatem numquam quod vero illum hic! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis vero ratione recusandae illum architecto harum accusamus dolor, quidem consequuntur facere fugiat cum, cumque quasi quisquam a tempore tenetur ab et? Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum debitis officia dolores itaque optio! Sapiente, nostrum perferendis? Dicta sunt ipsum commodi fuga placeat voluptate totam, quas a veniam. Aut, cupiditate. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore temporibus deserunt cumque pariatur ex officiis mollitia tempora architecto. Possimus facere minima dolorum sed officiis commodi aspernatur autem inventore fuga culpa. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae corporis tempore cupiditate quis facilis officia quia, cum laboriosam voluptas quas eos iusto laudantium sequi voluptatem numquam quod vero illum hic! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis vero ratione recusandae illum architecto harum accusamus dolor, quidem consequuntur facere fugiat cum, cumque quasi quisquam a tempore tenetur ab et? Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum debitis officia dolores itaque optio! Sapiente, nostrum perferendis? Dicta sunt ipsum commodi fuga placeat voluptate totam, quas a veniam. Aut, cupiditate. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore temporibus deserunt cumque pariatur ex officiis mollitia tempora architecto. Possimus facere minima dolorum sed officiis commodi aspernatur autem inventore fuga culpa. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae corporis tempore cupiditate quis facilis officia quia, cum laboriosam voluptas quas eos iusto laudantium sequi voluptatem numquam quod vero illum hic! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis vero ratione recusandae illum architecto harum accusamus dolor, quidem consequuntur facere fugiat cum, cumque quasi quisquam a tempore tenetur ab et? Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum debitis officia dolores itaque optio! Sapiente, nostrum perferendis? Dicta sunt ipsum commodi fuga placeat voluptate totam, quas a veniam. Aut, cupiditate. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore temporibus deserunt cumque pariatur ex officiis mollitia tempora architecto. Possimus facere minima dolorum sed officiis commodi aspernatur autem inventore fuga culpa. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae corporis tempore cupiditate quis facilis officia quia, cum laboriosam voluptas quas eos iusto laudantium sequi voluptatem numquam quod vero illum hic!';
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { user } = useAuth();
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceolder;
+
+  const fetchMovie = useCallback(async () => {
+    try {
+      const response = await api.get(`/movie-notes/${id}`);
+      setMovie(response.data);
+    } catch {
+      navigate('/not-found');
+    }
+  }, [id, navigate]);
+
+  useEffect(() => {
+    fetchMovie();
+  }, [fetchMovie]);
 
   return (
     <Container>
       <Header />
-      <Content>
-        <Button
-          title="voltar"
-          icon={FiArrowLeft}
-          type="button"
-          variant="text-button"
-          className="backBtn"
-          onClick={() => navigate(-1)}
-        />
+      {!!Object.keys(movie).length && (
+        <Content>
+          <Button
+            title="voltar"
+            icon={FiArrowLeft}
+            type="button"
+            variant="text-button"
+            className="backBtn"
+            onClick={() => navigate(-1)}
+          />
 
-        <div className="row-div">
-          <h3>O Menu</h3>
-          <Rating rating={4} size={20} />
-        </div>
+          <div className="row-div">
+            <h3>{movie.title}</h3>
+            <Rating rating={4} size={20} />
+          </div>
 
-        <div className="row-div">
-          <img src="https://github.com/lucasdoloh0v.png" alt="" />
-          <p>Por Lucas Alves</p>
+          <div className="row-div">
+            <img src={avatarUrl} alt="" />
+            <p>Por {user.name}</p>
 
-          <FiClock />
-          <p>23/08/2023 15:00</p>
-        </div>
+            <FiClock />
+            <p>{movie.created_at}</p>
+          </div>
 
-        <Tags>
-          {tags.map((tag) => (
-            <Tag name={tag} key={tag} />
-          ))}
-        </Tags>
+          <Tags>
+            {movie.movieTags.map((tag) => (
+              <Tag name={tag.name} key={tag.id} />
+            ))}
+          </Tags>
 
-        <p>{description}</p>
-      </Content>
+          <p>{movie.description}</p>
+        </Content>
+      )}
     </Container>
   );
 }
